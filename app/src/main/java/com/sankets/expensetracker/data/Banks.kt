0eab22bf459a2d0df43c598d2fce081160e17500
sanket099@github.com <sinhasanket099@gmail.com>
2023-09-06 12:19:16 +0530

    crashlytics

--- app/src/main/java/com/sankets/expensetracker/data/Banks.kt
+++ app/src/main/java/com/sankets/expensetracker/data/Banks.kt
@@ -1,9 +1,11 @@
 package com.sankets.expensetracker.data
 
-data class Banks(
+import java.io.Serializable
+
+data class Banks (
     val id: Int,
     val name: String,
     val icon: String,
     var isSelected: Boolean
-) {
+) : Serializable {
 }
